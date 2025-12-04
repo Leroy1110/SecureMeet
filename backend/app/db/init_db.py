21857d3eae9a7063f0c919769c24144367ff4fbc
Leroy1110 <leroyfainberg007@gmail.com>
2025-12-04 17:03:33 +0200

    feat: add init_db script to initialize the database, ignore db files

---
+++ backend/app/db/init_db.py
@@ -0,0 +1,10 @@
+import sqlalchemy as sa
+from app.db.base import Base
+from app.db.session import engine
+import app.db.models
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    init_db()
