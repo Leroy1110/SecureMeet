c7563836b738d26fee1ed22944e57e171e5ad445
Leroy1110 <leroyfainberg007@gmail.com>
2026-01-26 23:36:25 +0200

    feat(room_manager): extend RoomManager to manage room creation

--- backend/app/signaling/room_manager.py
+++ backend/app/signaling/room_manager.py
@@ -9,3 +9,19 @@ class RoomState:
         self.host_ws = None
         self.waiting_ws = set()
         self.active_ws = set()
+
+class RoomManager:
+    rooms: dict[str, RoomState]
+
+    def __init__(self):
+        self.rooms = {}
+
+    def get_or_create_room(self, room_code: str) -> RoomState:
+        if room_code in self.rooms:
+            return self.rooms[room_code]
+        
+        room_new = RoomState()
+
+        self.rooms[room_code] = room_new
+
+        return room_new
