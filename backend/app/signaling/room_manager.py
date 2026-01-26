75436a547ce0141e0a47234ca6088f5835803f04
Leroy1110 <leroyfainberg007@gmail.com>
2026-01-26 23:37:15 +0200

    feat(room_manager): implement connection management methods for WebSocket rooms

--- backend/app/signaling/room_manager.py
+++ backend/app/signaling/room_manager.py
@@ -25,3 +25,33 @@ class RoomManager:
         self.rooms[room_code] = room_new
 
         return room_new
+
+    def add_connection(self, room_code: str, ws: WebSocket, role: str, state: str):
+        room_state: RoomState = self.get_or_create_room(room_code)
+
+        if role == "host":
+            room_state.host_ws = ws
+        else:
+            if state == "waiting":
+                room_state.waiting_ws.add(ws)
+            elif state == "active":
+                room_state.active_ws.add(ws)
+            else:
+                raise ValueError(f"Invalid state: {state}")
+    
+    def remove_connection(self, room_code: str, ws: WebSocket):
+        if not room_code in self.rooms:
+            return
+        
+        room_state: RoomState = self.rooms[room_code]
+        room_active: set[WebSocket] = room_state.active_ws
+        room_waiting: set[WebSocket] = room_state.waiting_ws
+
+        if ws is room_state.host_ws:
+            room_state.host_ws = None
+        
+        room_active.discard(ws)
+        room_waiting.discard(ws)
+        
+        if room_state.host_ws is None and not room_active and not room_waiting:
+            del self.rooms[room_code]
