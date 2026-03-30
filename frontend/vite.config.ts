7aaceeb01a0b7dcafa14fb270dc4bf47b126cdf6
Leroy1110 <leroyfainberg007@gmail.com>
2026-03-30 15:57:12 +0300

    feat: configure HTTPS dev server (Vite) and exclude SSL cert files from git

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import fs from 'fs';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +10,12 @@ export default defineConfig({
     react(), 
     tailwindcss(),
   ],
+  server: {
+    host: true,
+    port: 5173,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "certs/localhost+3-key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "certs/localhost+3.pem"))
+    }
+  }
 })
